97ff88919e446f18d52438a728257f1cc712c1f1
Ibn Ahmad <justforlearningcode@gmail.com>
2019-01-10 12:59:38 +0100

    Initial Commit

---
+++ test/tdd/demo/StringCalculator1Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ibnahmad
+ */
+public class StringCalculator1Test {
+    
+    public StringCalculator1Test() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
